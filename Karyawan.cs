2ceea98f5035e035b66b8414a0c3085c20a93192
refdianshella <refdianshella@students.amikom.ac.id>
2022-04-20 09:28:25 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4462
+{
+    public class Karyawan
+    {
+        // constructor default
+        public Karyawan()
+        {
+
+        }
+
+        // constructor 3 parameter
+        public Karyawan(string nik, string nama, int gajiperbulan)
+        {
+            NIK = nik;
+            NAMA = nama;
+            Gajiperbulan = gajiperbulan;
+        }
+
+        // properties
+        public string NAMA { get; set; }
+        public string NIK { get; set; }
+        public int Gajiperbulan { get; set; }
+    }
+}
